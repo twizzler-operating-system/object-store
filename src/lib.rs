1e2aa920eabdf1513f16a9ca56592392d969ac80
Daniel Bittman <danielbittman1@gmail.com>
2025-06-12 16:05:48 -0700

    Remove old ext2 code.

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,10 @@ mod kms;
 mod lethe_object_store;
 mod wrapped_extent;
 
-mod ext2;
 mod ext4;
 pub use ext4::Ext4Store;
 pub mod paged_object_store;
 
-pub use ext2::Ext2ObjectStore;
 pub use lethe_object_store::{
     key_fprint, LetheObjectStore, LetheState, PerObjLetheState, StdIoWrapper as LetheIoWrapper,
 };
